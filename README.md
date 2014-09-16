ff0b93c4ef119bc57fa9045e8234d8e5a4483724
Martin Welß <mw@it-focus.de>
2014-09-16 21:18:35 +0200

    first release

--- README.md
+++ README.md
@@ -15,13 +15,26 @@ autotable takes a sample of the first 100 lines to guess the datatypes, which ar
 
 The script reads the database login from the file $HOME/system.properties, which should contain these three entries:
   
-  autotable.jdbc.url: jdbc:postgresql:example
-  autotable.user: wildfly
-  autotable.password: wildfly
+    autotable.jdbc.url: jdbc:postgresql:example
+    autotable.user: wildfly
+    autotable.password: wildfly
 
 To run the script you need to have Groovy installed and the JDBC-Driver of your choice at hand. So here's the commandline to import table books into postgres:
 
-  groovy -cp ../lib/postgresql-9.3-1101.jdbc41.jar autotable.groovy ../stuff/books.csv
+    groovy -cp ../lib/postgresql-9.3-1101.jdbc41.jar autotable.groovy ../stuff/books.csv
+
+Options
+-------
+
+At the beginning of the file you can set some options. For example to add data to an existing table, just set DROP\_TABLE and CREATE\_TABLE to false. 
   
+    GUESS_LINES=100
+    DROP_TABLE=true
+    CREATE_TABLE=true
+    STRING_DELIMITER=/\"/
+    FIELD_SEPARATOR=/;(?=([^\"]*\"[^\"]*\")*[^\"]*$)/    // ignore ; in quotes
+    PROPERTIES="system.properties"
+    DATE_FORMATS=["dd.MM.yyyy HH:mm:ss", "dd.MM.yyyy"]
+    SQL_DATE="yyyy-MM-dd HH:mm:ss"
   
   
---
+++ autotable.groovy
@@ -0,0 +1,147 @@
+#!/usr/bin/env groovy
+import groovy.transform.*
+import groovy.sql.Sql
+
+import java.nio.file.*
+
+// class for options to make them accessible by class ATColumn
+class ATOptions {
+	static GUESS_LINES=100
+	static DROP_TABLE=true
+	static CREATE_TABLE=true
+	static STRING_DELIMITER=/\"/
+	static FIELD_SEPARATOR=/;(?=([^\"]*\"[^\"]*\")*[^\"]*$)/    // ignore ; in quotes
+	static PROPERTIES="system.properties"
+	static DATE_FORMATS=["dd.MM.yyyy HH:mm:ss", "dd.MM.yyyy"]
+	static SQL_DATE="yyyy-MM-dd HH:mm:ss"
+}
+
+@Canonical
+class ATColumn {	
+	String name
+	String type // bigint, numeric, text, timestamp
+	boolean primary=false
+	static String BIGINT="bigint"
+	static String NUMERIC="numeric"
+	static String TEXT="text"
+	static String TIMESTAMP="timestamp"
+	
+	static void setMysqlTypes() {
+		NUMERIC="double"
+	}
+	
+	def addTypeSample(String value) {
+		value=value.trim().replaceAll(ATOptions.STRING_DELIMITER, "")
+		if(value.size()==0) { return }
+		def sample=TEXT
+		if(value.isNumber()) { sample=NUMERIC }
+		if(value.isLong()) { sample=BIGINT }
+		else {
+			ATOptions.DATE_FORMATS.each {
+				try {
+					Date(it,value)
+					sample=TIMESTAMP
+				}
+				catch(Exception x) { }
+			}
+		}
+		
+		if(type==null) { type=sample; return }
+		if(type==sample) { return }
+		if(type==NUMERIC && sample==BIGINT) { return }
+		if(type==BIGINT && sample==NUMERIC) { type=NUMERIC; return }
+		type=TEXT //type!=sample -> text
+	}
+	
+	def getDDL() {
+		return name+" "+type+(primary ? " primary key" : "")
+	}
+	
+	def getSQLValue(String field) {
+		if(field.length()==0) { return "null" }
+		field=field.trim().replaceAll(ATOptions.STRING_DELIMITER, "")
+		if(type==TEXT) {return "'"+field+"'" }
+		if(type==TIMESTAMP) {
+			for(def format: ATOptions.DATE_FORMATS) {
+				try {
+					def time=Date(format,field)
+					return "'"+time.format(ATOptions.SQL_DATE)+"'"
+				}
+				catch(Exception x) { }
+			}
+			return "null"
+		}
+		return field
+	}
+}
+
+
+tablename=Paths.get(args[0]).getFileName().toString().tokenize('.')[0]
+columns=[]
+
+sysprops=new File("${System.properties.'user.home'}/${ATOptions.PROPERTIES}")
+assert sysprops.exists()
+println("reading properties from $sysprops")
+sysprops.withInputStream {
+	System.properties.load(it)
+}
+
+println "connecting to database: ${System.properties.'autotable.jdbc.url'} as user ${System.properties.'autotable.user'}"
+def sql= Sql.newInstance("${System.properties.'autotable.jdbc.url'}",
+					"${System.properties.'autotable.user'}",
+					"${System.properties.'autotable.password'}")
+
+if((System.properties.'autotable.jdbc.url').toLowerCase().indexOf("mysql")>4) {
+	ATColumn.setMysqlTypes()
+}
+
+data=new File(args[0])
+
+// We analyze the first x lines to guess columntypes
+line=0;
+data.withReader { reader ->
+	while((row=reader.readLine())!=null) {
+		line++
+		row=row.trim()
+		if(row.length()==0) { continue }
+		if(line>ATOptions.GUESS_LINES) { break }
+		def fields=row.split(ATOptions.FIELD_SEPARATOR)
+		if(line==1) {
+			fields.each { columns << new ATColumn(it,null) }
+			columns[0].primary=true
+			continue
+		}
+		[fields, columns].transpose().each { field,column -> column.addTypeSample(field) }
+	}
+}
+
+if(ATOptions.DROP_TABLE) {
+	println "drop table $tablename"
+	sql.execute("drop table if exists "+tablename)
+}
+if(ATOptions.CREATE_TABLE) {
+	command="create table "+tablename
+	command+=" (" + columns*.getDDL().join(",") + ")"
+	println command
+	sql.execute(command)	
+}
+
+
+// read data
+line=0
+inserts=0
+data.splitEachLine(ATOptions.FIELD_SEPARATOR) { fields ->
+	line++
+	if(line>1 && fields.size()>1) {
+		command="insert into "+tablename
+		command+=" (" + columns*.name.join(",") + ") values "
+		vallist=[]
+		[fields, columns].transpose().each { field,column -> vallist << column.getSQLValue(field) }
+		command+=" (" + vallist.join(",") + ")"
+		//println command
+		sql.execute(command)
+		inserts++
+	}
+}
+println "== total lines: $line == inserts: $inserts =="
+sql.close()
